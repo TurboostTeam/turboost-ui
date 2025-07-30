320103f25cfc48f3347ba3ca0cbb041fc6758017
chenxuanzong <2622009279@qq.com>
2025-07-30 15:13:32 +0800

    fix: 修改

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,12 @@
 import type { NextConfig } from "next";
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
-const handleDevelopment = async () => {
-  try {
-    await setupDevPlatform();
-  } catch (err) {
-    console.error("Failed to setup development platform:", err);
-  }
-};
-
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
 if (process.env.NODE_ENV === "development") {
-  handleDevelopment();
+  await setupDevPlatform();
 }
 
 export default nextConfig;
