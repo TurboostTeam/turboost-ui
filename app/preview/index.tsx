2ac1ff9f215d71e4b6ec436eb56db1f27a58bc1a
cxz199705 <48152698+cxz199705@users.noreply.github.com>
2025-08-01 10:15:00 +0800

    feat: 新增 Filter 组件和 Input 组件 (#3)

--- app/preview/index.tsx
+++ app/preview/index.tsx
@@ -1,9 +1,13 @@
+import { FilterPreview } from "./filter";
+import { InputPreview } from "./input";
 import { TablePreview } from "./table";
 import { TagsInputPreview } from "./tags-input";
 import { ViewPreview } from "./view";
 
 export const components = [
   { name: "Tags Input", type: "表单", content: <TagsInputPreview /> },
+  { name: "Input", type: "表单", content: <InputPreview /> },
   { name: "View", type: "视图", content: <ViewPreview /> },
   { name: "Table", type: "视图", content: <TablePreview /> },
+  { name: "Filter", type: "视图", content: <FilterPreview /> },
 ];
